8c3058c7c4b6831b063553f76428488da2be8b79
Aaron Schwartz <aschwartz628@gmail.com>
2016-11-18 20:19:32 -0500

    implemented get all posts route

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var express = require('express');
-
 var server = express();
+var mongoose = require('mongoose');
+var postRouter = require('./server/routers/post.router.js');
 
 var port = process.env.PORT || 8080;
+var mongoURI = process.env.mongoURI || require('./config.js').mongoURI;
 
+mongoose.connect(mongoURI); // establish the connection to the mongo database
 
+server.use(postRouter); 
 
 server.get('/', function(req, res){
   res.send('I am working!');
