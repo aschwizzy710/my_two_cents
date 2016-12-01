b047484f48499c877da6c45a2053f812501182dc
Aaron Schwartz <aschwartz628@gmail.com>
2016-11-30 20:07:49 -0500

    changed the signup procedure to use the salt and hash storage methods

--- server/routers/user.router.js
+++ server/routers/user.router.js
@@ -17,6 +17,13 @@ router.get('/users/profile/:userID', function(req, res){
 });
 router.post('/users/signup', function(req, res){
   var user = new User(req.body);
+  if(!req.body.password){
+    return res.status(400).json({
+      msg: 'Bad Request - Signups require passwords'
+    });
+  }
+  // check to see if an email is on req.body. If not, send 400 with a message
+  user.setPassword(req.body.password);
   user.save(function(err){
     if(err){
       return res.status(500).json({
