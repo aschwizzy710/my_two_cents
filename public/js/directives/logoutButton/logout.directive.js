8f96e4a0575220d17039935c5b133cf1537f3308
Aaron Schwartz <aschwartz628@gmail.com>
2016-12-05 18:57:30 -0500

    created the logout button directive/component that will allow us to logout anywhere and everywhere

--- public/js/directives/logoutButton/logout.directive.js
+++ public/js/directives/logoutButton/logout.directive.js
@@ -5,6 +5,11 @@
   LogoutButtonDirective.$inject = [];
 
   function LogoutButtonDirective(){
-    
+    return {
+      restrict: 'EA', // this makes it available as a tag or attribute
+      scope: {}, // isolate scope -- keeps parent scope from contaminating
+      templateUrl: 'js/directives/logoutButton/logout.view.html',
+      controller: 'LogoutController'
+    };
   }
 }());
